34c61e6b5cee44367a09e092aa0508324dee46bd
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-09 21:48:24 +0300

    select best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Is a script that lists all records with  a score >= 10 in the table second_table
+-- query used to select by condition with order
+SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
