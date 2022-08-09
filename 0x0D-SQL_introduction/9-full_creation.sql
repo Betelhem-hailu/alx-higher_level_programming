7ad0724805e7df7887894fce30f8e9851d9ae48f
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-10 00:16:38 +0300

    create and insert

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
 -- Is a script that creats a table second_table and add multiples rows
 -- query used to create table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT) VALUES (1, "JHON", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "JHON", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8);
