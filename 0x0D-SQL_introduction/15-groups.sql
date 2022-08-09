0009605067d54b8638013570e9dd8024f35d1d4d
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-10 00:29:28 +0300

    number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Is a script that lists the number of records with the same score int the second_table in my MYSQL server
 -- query used to count
-SELECT score, COUNT(score) AS "number" FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS "number" FROM second_table GROUP BY score ORDER BY COUNT(score) DESC;
