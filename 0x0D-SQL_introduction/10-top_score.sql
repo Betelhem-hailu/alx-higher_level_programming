a36ff49eb17ec0631be3519707152eae6d7f9fce
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-09 21:41:23 +0300

    sorted list

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Is a script that lists all records of the table second_table 
 -- query used to select by condition
-SELECT name, score FROM second_table ORDER BY score ASC;
+SELECT name, score FROM second_table ORDER BY score DESC ;
