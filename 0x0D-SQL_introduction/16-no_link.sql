913b013bf1cd41f7c96ed9cddcef50bd3bc3cb29
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-10 00:46:47 +0300

    omit null record

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Is a script that lists all records of the table second_table in my MYSQL server
 -- query used to omit no value record
-SELECT score, name FROM second_table WHERE name = "%like%" ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE `name` IS NOT NULL ORDER BY score DESC;
