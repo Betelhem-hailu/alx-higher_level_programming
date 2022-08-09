421ac1dd0e244d16f6cccc8f3dd0b0147809041d
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-09 20:50:18 +0300

    count records

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- IS a script that displays that number of records with id=89 in the table
 -- query used to count
-SELECT (COUNT) * FROM first_table WHERE id = 89;
+SELECT COUNT (*) FROM first_table WHERE id = 89;
