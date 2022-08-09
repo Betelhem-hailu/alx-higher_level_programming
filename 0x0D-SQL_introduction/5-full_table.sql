d7e3f6f814ad4ad22744768887e6b6307589b771
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-09 20:07:52 +0300

    describe table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Is a script that prints the full description of the table first_table from the database hbtn_0c_0 in my MYSQL server
 -- query used to describe table
-DESC first_table;
+DESCRIBE first_table;
