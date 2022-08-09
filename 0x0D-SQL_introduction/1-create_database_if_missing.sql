50df1a23699f5e5da1e5990a8f644c110c4e122c
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-08-09 19:41:28 +0300

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Is script that creates the database hbtn_0c_0 in my MYSQL server
 -- query used to create database
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
