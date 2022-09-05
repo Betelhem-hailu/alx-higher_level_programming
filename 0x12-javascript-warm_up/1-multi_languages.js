8d7394008bd663418abd0912552e5f6f15dffd64
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-09-05 19:06:06 +0300

    printing 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const line1 = "C is fun";
-const line2 = "python is cool";
-const line3 = "JavaScript is amazing";
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
